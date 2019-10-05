5995b0de26001597373e0be7b9ee5ca9d6ed8968
Andre <joelbong@hotmail.com>
2019-10-05 08:08:55 +0200

    Updates

---
+++ precache-manifest.0271643ed356ad1f7740428a8f264c86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a952f4c3e2b6fd2e8ae7ca051ccddc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e2df7df4e2294eff772c",
+    "url": "/monsters-rolodex/static/css/main.8d590d0d.chunk.css"
+  },
+  {
+    "revision": "f48ddc321a3f07929ab2",
+    "url": "/monsters-rolodex/static/js/2.900ae16e.chunk.js"
+  },
+  {
+    "revision": "e2df7df4e2294eff772c",
+    "url": "/monsters-rolodex/static/js/main.b04810a7.chunk.js"
+  },
+  {
+    "revision": "5242ad666bd6f2eaa34c",
+    "url": "/monsters-rolodex/static/js/runtime-main.dfe8ccc3.js"
+  }
+]);
